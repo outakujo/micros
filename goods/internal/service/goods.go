84111e64a8bdce642dedb38268ba9c37397145bb
wzx <431225046@qq.com>
2023-02-08 21:29:02 +0800

    update

--- goods/internal/service/goods.go
+++ goods/internal/service/goods.go
@@ -5,11 +5,11 @@ import (
 	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
 	"goods/internal/biz"
 
-	pb "goods/api/v1"
+	v1 "goods/api/v1"
 )
 
 type GoodsService struct {
-	pb.UnimplementedGoodsServer
+	v1.UnimplementedGoodsServer
 	biz *biz.GoodsBiz
 	dis *etcd.Registry
 }
@@ -18,7 +18,7 @@ func NewGoodsService(biz *biz.GoodsBiz, dis *etcd.Registry) *GoodsService {
 	return &GoodsService{biz: biz, dis: dis}
 }
 
-func (s *GoodsService) CreateGoods(ctx context.Context, req *pb.CreateGoodsRequest) (*pb.Model, error) {
+func (s *GoodsService) CreateGoods(ctx context.Context, req *v1.CreateGoodsRequest) (*v1.Model, error) {
 	goods := biz.Goods{
 		Name:  req.Name,
 		Price: req.Price,
@@ -28,40 +28,40 @@ func (s *GoodsService) CreateGoods(ctx context.Context, req *pb.CreateGoodsReque
 	if err != nil {
 		return nil, err
 	}
-	return &pb.Model{
+	return &v1.Model{
 		Id:    int64(create.ID),
 		Name:  create.Name,
 		Price: create.Price,
 		Count: create.Count,
 	}, nil
 }
-func (s *GoodsService) UpdateGoods(ctx context.Context, req *pb.UpdateGoodsRequest) (*pb.Model, error) {
-	return &pb.Model{}, nil
+func (s *GoodsService) UpdateGoods(ctx context.Context, req *v1.UpdateGoodsRequest) (*v1.Model, error) {
+	return &v1.Model{}, nil
 }
-func (s *GoodsService) DeleteGoods(ctx context.Context, req *pb.DeleteGoodsRequest) (*pb.DeleteGoodsReply, error) {
+func (s *GoodsService) DeleteGoods(ctx context.Context, req *v1.DeleteGoodsRequest) (*v1.DeleteGoodsReply, error) {
 	err := s.biz.Delete(ctx, int(req.Id))
-	return &pb.DeleteGoodsReply{}, err
+	return &v1.DeleteGoodsReply{}, err
 }
-func (s *GoodsService) GetGoods(ctx context.Context, req *pb.GetGoodsRequest) (*pb.Model, error) {
+func (s *GoodsService) GetGoods(ctx context.Context, req *v1.GetGoodsRequest) (*v1.Model, error) {
 	get, err := s.biz.Get(ctx, int(req.Id))
 	if err != nil {
 		return nil, err
 	}
-	return &pb.Model{
+	return &v1.Model{
 		Id:    int64(get.ID),
 		Name:  get.Name,
 		Price: get.Price,
 		Count: get.Count,
 	}, nil
 }
-func (s *GoodsService) ListGoods(ctx context.Context, req *pb.ListGoodsRequest) (*pb.ListGoodsReply, error) {
+func (s *GoodsService) ListGoods(ctx context.Context, req *v1.ListGoodsRequest) (*v1.ListGoodsReply, error) {
 	p, err := s.biz.List(ctx, int(req.Page), int(req.Size))
 	if err != nil {
 		return nil, err
 	}
-	models := make([]*pb.Model, 0)
+	models := make([]*v1.Model, 0)
 	for _, g := range p.List {
-		m := &pb.Model{
+		m := &v1.Model{
 			Id:    int64(g.ID),
 			Name:  g.Name,
 			Price: g.Price,
@@ -69,7 +69,7 @@ func (s *GoodsService) ListGoods(ctx context.Context, req *pb.ListGoodsRequest) 
 		}
 		models = append(models, m)
 	}
-	return &pb.ListGoodsReply{
+	return &v1.ListGoodsReply{
 		List:    models,
 		MaxPage: p.MaxPage,
 		Count:   p.Count,
--- user/internal/service/user.go
+++ user/internal/service/user.go
@@ -9,12 +9,11 @@ import (
 	"user/internal/biz"
 	"user/internal/conf"
 
-	pb "user/api/v1"
 	v1 "user/api/v1"
 )
 
 type UserService struct {
-	pb.UnimplementedUserServer
+	v1.UnimplementedUserServer
 	biz *biz.UserBiz
 	dis *etcd.Registry
 	log *log.Helper
@@ -25,7 +24,7 @@ func NewUserService(biz *biz.UserBiz, dis *etcd.Registry, au *conf.Auth, logger 
 	return &UserService{biz: biz, dis: dis, log: log.NewHelper(logger), au: au}
 }
 
-func (s *UserService) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.Model, error) {
+func (s *UserService) CreateUser(ctx context.Context, req *v1.CreateUserRequest) (*v1.Model, error) {
 	if md, ok := metadata.FromServerContext(ctx); ok {
 		// 从header里token值
 		extra := md.Get("token")
@@ -39,13 +38,13 @@ func (s *UserService) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.Model{
+	return &v1.Model{
 		Username: create.Username,
 		Nickname: create.Nickname,
 		Id:       int64(create.ID),
 	}, nil
 }
-func (s *UserService) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.Model, error) {
+func (s *UserService) UpdateUser(ctx context.Context, req *v1.UpdateUserRequest) (*v1.Model, error) {
 	timeout, cancelFunc := context.WithTimeout(context.Background(), time.Second)
 	defer cancelFunc()
 	client, err := goodsClient(s.dis, s.au)
@@ -56,43 +55,43 @@ func (s *UserService) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.Model{
+	return &v1.Model{
 		Username: getGoods.Name,
 	}, nil
 }
-func (s *UserService) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserReply, error) {
+func (s *UserService) DeleteUser(ctx context.Context, req *v1.DeleteUserRequest) (*v1.DeleteUserReply, error) {
 	err := s.biz.Delete(ctx, req.Username)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.DeleteUserReply{}, nil
+	return &v1.DeleteUserReply{}, nil
 }
-func (s *UserService) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.Model, error) {
+func (s *UserService) GetUser(ctx context.Context, req *v1.GetUserRequest) (*v1.Model, error) {
 	get, err := s.biz.Get(ctx, req.Username)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.Model{
+	return &v1.Model{
 		Username: get.Username,
 		Nickname: get.Nickname,
 		Id:       int64(get.ID),
 	}, nil
 }
-func (s *UserService) ListUser(ctx context.Context, req *pb.ListUserRequest) (*pb.ListUserReply, error) {
+func (s *UserService) ListUser(ctx context.Context, req *v1.ListUserRequest) (*v1.ListUserReply, error) {
 	p, err := s.biz.List(ctx, int(req.Page), int(req.Size))
 	if err != nil {
 		return nil, err
 	}
-	models := make([]*pb.Model, 0)
+	models := make([]*v1.Model, 0)
 	for _, user := range p.List {
-		m := &pb.Model{
+		m := &v1.Model{
 			Id:       int64(user.ID),
 			Username: user.Username,
 			Nickname: user.Nickname,
 		}
 		models = append(models, m)
 	}
-	return &pb.ListUserReply{
+	return &v1.ListUserReply{
 		List:    models,
 		MaxPage: p.MaxPage,
 		Count:   p.Count,
